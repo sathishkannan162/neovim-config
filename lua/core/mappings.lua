098d424cd7da3141d24cc6d7321c657ce6602c50
Sathish Kannan <sathishkannan162@gmail.com>
2024-07-12 16:57:59 +0530

    feat: show telescope document symbols

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -274,6 +274,7 @@ M.telescope = {
     ["<leader>fa"] = { "<cmd> Telescope find_files follow=true no_ignore=true hidden=true <CR>", "Find all" },
     ["<leader>fw"] = { "<cmd> Telescope live_grep <CR>", "Live grep" },
     ["<leader>fb"] = { "<cmd> Telescope buffers <CR>", "Find buffers" },
+    ["<leader>fd"] = { "<cmd> Telescope lsp_document_symbols <CR>", "Find in document symbols" },
     ["<leader>fh"] = { "<cmd> Telescope help_tags <CR>", "Help page" },
     ["<leader>fo"] = { "<cmd> Telescope oldfiles <CR>", "Find oldfiles" },
     ["<leader>fz"] = { "<cmd> Telescope current_buffer_fuzzy_find <CR>", "Find in current buffer" },
