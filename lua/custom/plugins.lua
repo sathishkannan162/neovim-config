056bf9e984ef73908fabd80563b3a3c7597bdc32
Sathish Kannan <sathishkannan162@gmail.com>
2024-10-01 21:03:30 +0530

    Add setup for trouble nvim

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -268,7 +268,27 @@ local plugins = {
   },
   {
     "folke/trouble.nvim",
+    event = "VeryLazy",
     dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("trouble").setup()
+      local actions = require "telescope.actions"
+      local open_with_trouble = require("trouble.sources.telescope").open
+
+      -- Use this to add more results without clearing the trouble list
+      local add_to_trouble = require("trouble.sources.telescope").add
+
+      local telescope = require "telescope"
+
+      telescope.setup {
+        defaults = {
+          mappings = {
+            i = { ["<c-t>"] = open_with_trouble },
+            n = { ["<c-t>"] = open_with_trouble },
+          },
+        },
+      }
+    end,
   },
   {
     "ThePrimeagen/harpoon",
